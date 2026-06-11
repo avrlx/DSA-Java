56633ca6b672c9d3e74c9361efe1bb0b40372f81
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-12 00:32:42 +0530

    new

---
+++ If-Else/AbsoluteValue.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        double n = sc.nextDouble();
+        if(n<0){
+            double a = n * -1;
+            System.out.println("The absolute value of " + n + " is " + a);
+        }
+        else System.out.println("The absolute value of " + n + " is " + n);
+
+    }
+    
+}
---
+++ If-Else/EvenOdd.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int n = sc.nextInt();
+        if(n%2==0) System.out.println(n+" is Even number.");
+        else System.out.println(n + " is Odd number.");
+    }
+    
+}
