5e20a1f09afa41c9e5ff957139436ea20b169139
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-12 22:26:47 +0530

    ap
    Arithmetic Progression

---
+++ Loops/APFofNterms.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class APFofNterms {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number of terms : ");
+        int n = sc.nextInt();
+        int last = 1 + 2 * (n - 1);
+        System.out.println("Arithmetic Progression from 1 to " + last + " (" + n + " terms)");
+        for (int i = 1; i <= n; i++) {
+            int term = (i-1) * 2 + 1;
+            System.out.println( i + " Term - " + term);
+            
+        }
+    }
+}
