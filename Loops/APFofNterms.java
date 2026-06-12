fc3a983354fce6637a8d1ea568404d837b2498d4
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 00:09:39 +0530

    Update APFofNterms.java
    modified

--- Loops/APFofNterms.java
+++ Loops/APFofNterms.java
@@ -7,7 +7,7 @@ public class APFofNterms {
         System.out.print("Enter Number of terms : ");
         int n = sc.nextInt();
         int last = 1 + 2 * (n - 1);
-        System.out.println("Arithmetic Progression from 1 to " + last + " (" + n + " terms)");
+        System.out.println("Arithmetic Progression from 1 to " + last + " (" + n + " terms) with common diff of 2");
         for (int i = 1; i <= n; i++) {
             int term = (i-1) * 2 + 1;
             System.out.println( i + " Term - " + term);
