420acec4a9593506a92fcaa8440b6215d9c357b5
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-12 20:54:31 +0530

    Delete Loops directory

--- Loops/PrintNumber.java
+++
@@ -1,8 +0,0 @@
-public class PrintNumber {
-    public static void main(String[] args) {
-        for(int var1 = 1; var1 <= 10; ++var1) {
-         System.out.print(var1 + " ");
-        }
-    }
-    
-}
