992d2a4f07458099d7c84625f48145d76e32e503
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 00:51:29 +0530

    Create PrintPositive.java
    Print this series - 99,95,91,87,.. upto all terms which are positive

---
+++ Loops/PrintPositive.java
@@ -0,0 +1,10 @@
+public class PrintPositive {
+    public static void main(String[] args) {
+        System.out.println("Start from 99 till positive number with diff of 4");
+        for (int i = 99; i >= 0; i -= 4) {
+            System.out.print(i + " ");
+            
+        }
+    }
+    
+}
