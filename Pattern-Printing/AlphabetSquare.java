ee85570827b65d7fa5259541bb6f15726e0e18f4
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 15:32:02 +0530

    Alphabet Square

---
+++ Pattern-Printing/AlphabetSquare.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any no : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 65; j <= n + 64; j++) {
+                System.out.print( (char)j + " ");
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
