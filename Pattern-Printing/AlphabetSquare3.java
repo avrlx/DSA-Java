11c7f50c1a64905d3591934f090aa90852d96849
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 15:46:46 +0530

    Alphabet Square 3

---
+++ Pattern-Printing/AlphabetSquare3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AlphabetSquare3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any no : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char)(i+64) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
