775a110a2deebed9077f0025bdf4660909920522
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 10:03:26 +0530

    fix

--- Loops/FactorOfNOP.java
+++ Loops/FactorOfNOP.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class FactorOfNOP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter any Nuber : ");
+        System.out.print("Enter any Number : ");
         int n = sc.nextInt();
         System.out.println("Factors of " + n);
         for (int i = 1; i <= Math.sqrt(n); i++) {
