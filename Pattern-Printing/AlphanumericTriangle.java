81acabc2143a7544fea01e361a12fc2c80ea23b9
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-14 00:03:07 +0530

    Create AlphanumericTriangle.java

---
+++ Pattern-Printing/AlphanumericTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AlphanumericTriangle {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if(i % 2 == 0){
+                for (int j = 1; j <= i; j++) {
+                    System.out.print((char)(j+64) + " ");
+                }
+            }
+            else {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
