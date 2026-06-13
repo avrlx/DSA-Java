de2f529ab316d8b569b641f7c9b33d7e532efbe3
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 10:45:37 +0530

    some change

--- Loops/CountDigit.java
+++ Loops/CountDigit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DigitOfNuber {
+public class CountDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any number : ");
