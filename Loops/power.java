a2933415d54d51749f94518599563a091ce9d964
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 12:34:43 +0530

    End of Loops

--- Loops/power.java
+++ Loops/power.java
@@ -6,7 +6,7 @@ public class power {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Base number : ");
         int a = sc.nextInt();
-        System.out.print("Enter Power of Base number : ");
+        System.out.print("Enter Power of " + a + " : ");
         int b = sc.nextInt();
         int power = 1;
         for (int i = 1; i <= b; i++) {
