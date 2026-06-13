5a8820cc32094d0e0212c0508c1826e1cc0fb9ed
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 09:37:00 +0530

    Create CompositeNubere.java
    WAP to check if a given number is composite or not.

---
+++ Loops/CompositeNubere.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class CompositeNubere {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any Number : ");
+        int n = sc.nextInt();
+        boolean flag = false;
+        for (int i = 2; i <= n-1; i++) {
+            if(n % i == 0){
+                flag = true;
+                break;
+            }
+            
+        }
+        if(flag) System.out.println(n + " is Composite Number.");
+        else System.out.println(n + " is not Composite Number.");
+    }
+    
+}
