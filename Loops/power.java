6fa9319f0ff637df9c2cc2b34210420716278afd
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 12:31:21 +0530

    Create power.java
    'a' raise to the power 'b'.

---
+++ Loops/power.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Base number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Power of Base number : ");
+        int b = sc.nextInt();
+        int power = 1;
+        for (int i = 1; i <= b; i++) {
+            power *= a;
+            
+        }
+        System.out.println(a + " to the Power " + b + " is " + power + ".");
+
+    }
+    
+}
