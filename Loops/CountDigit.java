8d4566d4a3c67ea297856da25a7688407db1356c
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-13 10:53:45 +0530

    Update CountDigit.java

--- Loops/CountDigit.java
+++ Loops/CountDigit.java
@@ -6,6 +6,8 @@ public class CountDigit {
         System.out.print("Enter any number : ");
         int n = sc.nextInt();
         int temp = n;
+        if (n == 0) n = 9; // if n == 0 the if will make n = (any single digit number) , so that loop run for 1 times and count = 1.
+        // b/c 0 is a one digit number.
         int count = 0;
         while(n != 0){
             n /=10;
