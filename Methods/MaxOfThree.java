09b0c942d55a25eb359e4ff9872cd09836e8b90e
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-14 22:15:37 +0530

    Create MaxOfThree.java

---
+++ Methods/MaxOfThree.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class MaxOfThree {
+    public static int max(int a, int b, int c){
+        return Math.max(a,Math.max(b,c));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three number : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Max of " + a + " , " + b + " and " + c + " is " + max(a,b,c));
+
+
+    }
+}
