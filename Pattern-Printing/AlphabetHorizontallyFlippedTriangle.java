8d1b983989f1cc3350420e47f354e6a7de181524
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-14 08:23:08 +0530

    Alphabet Horizontally Flipped Triangle

---
+++ Pattern-Printing/AlphabetHorizontallyFlippedTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AlphabetHorizontallyFlippedTriangle {
+     public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter N : ");
+            int n = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n-i+1; j++) {
+                    System.out.print((char)(j+64) + " "); 
+                }
+                System.out.println();
+            }
+    }
+}
