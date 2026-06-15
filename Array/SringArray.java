ecd38a5b82ea0b41390d8dfacac75cee5e2efa0c
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 21:34:48 +0530

    Create SringArray.java

---
+++ Array/SringArray.java
@@ -0,0 +1,9 @@
+public class SringArray {
+    public static void main(String[] args) {
+        String[] a = { "Aviral" , "Singh" , "is" ,"Btech" , "CSE" ,  "Student"};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+    
+}
