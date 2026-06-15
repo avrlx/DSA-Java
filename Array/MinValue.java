9c2becda876482d4bf33a285b58d74c40fe260ba
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 21:00:01 +0530

    Create MinValue.java

---
+++ Array/MinValue.java
@@ -0,0 +1,8 @@
+public class MinValue {
+    public static void main(String[] args) {
+        int x = Integer.MIN_VALUE;
+        System.out.println("Minimum value of Integer : " + x);
+        // -2147483648
+    }
+    
+}
