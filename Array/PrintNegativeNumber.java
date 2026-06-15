fc683e802c4017bd9582171b3d0af0e16994f954
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 20:40:30 +0530

    Create PrintNegativeNumber.java

---
+++ Array/PrintNegativeNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintNegativeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number of elements : ");
+        int n = sc.nextInt();
+        System.out.print("Enter "+n+" element : ");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Element are : ");
+        for (int i = 0; i < arr.length; i++) {
+             if(arr[i] < 0){
+            System.out.print(arr[i] + " ");
+             }
+        }
+    }
+}
