22216b6f87ddb888ff6903fb6cbf154217479031
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 21:00:08 +0530

    Create MaxValue.java

---
+++ Array/MaxValue.java
@@ -0,0 +1,8 @@
+public class MaxValue {
+     public static void main(String[] args) {
+        int x = Integer.MAX_VALUE;
+        System.out.println("Maximum value of Integer : " + x);
+        //  2147483647
+    }
+    
+}
