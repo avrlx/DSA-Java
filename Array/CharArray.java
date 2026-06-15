8ad5ba46280e060eba670a4f00988973c25f46ed
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 21:34:39 +0530

    Create CharArray.java

---
+++ Array/CharArray.java
@@ -0,0 +1,9 @@
+public class CharArray {
+    public static void main(String[] args) {
+        char[] a = {'a' , 'v' , 'i', 'r' ,'a' , 'l'};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+    
+}
