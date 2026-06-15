69682282b2df6532265bab03e598da80f69438de
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 13:57:49 +0530

    Create Printing.java

---
+++ Array/Printing.java
@@ -0,0 +1,9 @@
+public class Printing {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,6,7,5,8,9,0,2,1};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+    
+}
