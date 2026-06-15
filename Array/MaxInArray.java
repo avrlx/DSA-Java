b3aaa4dbdebc38db234d344fff69a86f61655f99
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 20:55:04 +0530

    Create MaxInArray.java

---
+++ Array/MaxInArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number of elements : ");
+        int n = sc.nextInt();
+        System.out.print("Enter "+n+" element : ");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0] ;
+        for (int i = 0; i < arr.length; i++) {
+            if(max < arr[i])
+                max = arr[i];
+        }
+        System.out.println("Maximum element of Array : " + max);
+    }
+    
+}
