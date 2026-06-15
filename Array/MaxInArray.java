1aa0be2d76a40088b2acd2337e9468b2a6b7e30a
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-15 20:59:52 +0530

    Update MaxInArray.java

--- Array/MaxInArray.java
+++ Array/MaxInArray.java
@@ -10,7 +10,7 @@ public class MaxInArray {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        int max = arr[0] ;
+        int max = Integer.MIN_VALUE ;
         for (int i = 0; i < arr.length; i++) {
             if(max < arr[i])
                 max = arr[i];
