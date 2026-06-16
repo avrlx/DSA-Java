0604df0d75824c68a243d9d368653ed491e56bea
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-16 12:18:52 +0530

    Create ForEachLoop.java

---
+++ Array/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5, 6, 7, 8, 7, 3, 9, 7, 3, 6, 8, 3, 10, 23, 11, 14 };
+        for (int ele : a) {
+            System.out.print(ele + " ");
+        }
+    }
+    
+}
