bf815e3fbec93b5f500366e345a99f1048d800e6
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-18 10:39:08 +0530

    Update Basic.java

--- Array/Array-List/Basic.java
+++ Array/Array-List/Basic.java
@@ -11,20 +11,12 @@ public class Basic {
         arr.add(67); // 2
         arr.add(56); // 3
         arr.add(29); // 4
-        System.out.println(arr);
-        
-        // Update the value..
-        arr.set(2,4);
-        
-        System.out.println(arr.get(1));
-        arr.add(2, 30); // insert element in 2nd index..
 
         // print complete arraylist..
         System.out.println(arr);
 
-        arr.remove(3); //remove at index 3..
-        
-        System.out.println(arr); // 67 removed
-
+        for( int ele : arr){
+            System.out.print(ele + " "); // print for each loop ..
+        }
     }
 }
