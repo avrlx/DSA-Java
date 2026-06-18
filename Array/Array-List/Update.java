615bd8c050a44b924e5d98d34b4bfe6c3ed8b917
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-18 10:30:22 +0530

    Update Update.java

--- Array/Array-List/Update.java
+++ Array/Array-List/Update.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 
 public class Update {
     public static void main(String[] args) {
-        ArrayList var1 = new ArrayList();
-        var1.add(10);
-        var1.add(93);
-        var1.add(67);
-        var1.add(56);
-        var1.add(29);
-        System.out.println(var1); 
+         ArrayList <Integer> arr = new ArrayList<>();
+        arr.add(10); // 0
+        arr.add(93); // 1
+        arr.add(67); // 2
+        arr.add(56); // 3
+        arr.add(29); // 4
+        System.out.println(arr); 
         // Update the value..
-        var1.set(2,4); // change index 2 to 4..
-        System.out.println(var1); 
+        arr.set(2,4); // change index 2 to 4..
+        System.out.println(arr); 
     }
 }
