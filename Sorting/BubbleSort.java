e7138ee20e45b8911a859f728cf6590e81eee211
avrlx <216227742+avrlx@users.noreply.github.com>
2026-06-19 21:26:17 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a  = {1, 3 ,5 ,6, 7, 8,7,3,9,7,3,6,8,3};
+        System.out.print("Array before Sorting : ");
+        print(a);
+        for (int i = 0; i < a.length -1 ; i++) {
+            for (int j = 0; j < a.length -1 - i; j++) {
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        System.out.print("\nArray after Sorting : ");
+        print(a);
+    }
+
+    public static void print(int[] a) {
+        
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+}
