6287cf0b418732cf7b338d5141d455dcf49fb121
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-16 00:47:23 +0530

    concatination

---
+++ String/ConcatWithPlus.java
@@ -0,0 +1,15 @@
+package String;
+
+public class ConcatWithPlus {
+    static void main(String[] args) {
+        String s = "aviral ";
+        String s2 = "singh";
+        String s3 = s + s2;
+        System.out.println(s3);
+        System.out.println(10 + 20 + s3);
+        System.out.println( s3 + 10 + 20  );
+        System.out.println(10 + s3+ 20 );
+        System.out.println( s3 + (10 + 20) );
+        System.out.println('A' + 10 + s3);
+    }
+}
---
+++ String/Concatination.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Concatination {
+    static void main(String[] args) {
+        String s = "aviral ";
+        String s2 = "singh";
+        String s3 = s.concat(s2);
+        System.out.println(s3);
+
+    }
+}
