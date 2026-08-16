629e3e549f0cc25ffb567392cf27216a8f224c1e
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-16 12:10:32 +0530

    Update NewString.java

--- String/NewString.java
+++ String/NewString.java
@@ -3,6 +3,11 @@ package String;
 public class NewString {
     static void main(String[] args) {
         String s = new String("hellow");
-        System.out.println(s);
+        System.out.println(s);// string in java is immutable we cannot change the string .
+        // when we concat string it make a new sting after joining them..
+        // if we make two string with same value they point the same location until we use the new keyword to make a another copy at other location..
+        String s1 = "aviral";
+        String s2 = "aviral";
+        System.out.println(s1==s2);
     }
 }
