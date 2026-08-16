630c86f8597f2b4a572c50ad92cadbc3354ced2c
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-16 12:05:39 +0530

    Create SringEqual.java

---
+++ String/SringEqual.java
@@ -0,0 +1,10 @@
+package String;
+
+public class SringEqual {
+    static void main(String[] args) {
+        String s = new String("aviral");
+        String str = new String("aviral");
+        System.out.println(s == str); // this compare the location of string not character
+        System.out.println(s.equals(str)); // it compare the string char by char..
+    }
+}
