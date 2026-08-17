b5d54c0cc36801eedd89fef91690f7cb6749a92d
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-17 19:26:35 +0530

    Create StingBuilder.java

---
+++ String/StingBuilder.java
@@ -0,0 +1,16 @@
+package String;
+
+public class StingBuilder {
+    static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.length() + " " + sb.capacity());
+        sb.append("hellow"); // we can add text from behind
+        System.out.println(sb);
+        sb.setCharAt(2,'i'); // we can edit the stringbuilder
+        System.out.println(sb);
+        String s = sb.toString(); // we can change to string also
+        System.out.println(s);
+
+
+    }
+}
