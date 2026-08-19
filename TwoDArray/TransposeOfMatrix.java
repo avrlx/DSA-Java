85f7e3948caae6546f4cd0216d779a18d4b645a3
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-19 19:46:54 +0530

    Create TransposeOfMatrix.java

---
+++ TwoDArray/TransposeOfMatrix.java
@@ -0,0 +1,27 @@
+package TwoDArray;
+
+public class TransposeOfMatrix {
+    static void main(String[] args) {
+        int [][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <=i ; j++) {
+                if(i!=j) {
+                    int temp = arr[i][j];
+                    arr[i][j] = arr[j][i];
+                    arr[j][i] = temp;
+                }
+            }
+
+        }
+        for(int[] a : arr){
+            for(int b :a){
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
+    }
+}
