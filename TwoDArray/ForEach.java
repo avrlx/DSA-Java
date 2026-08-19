4a1db8a2c314db37432be36b6d4f8f8ade8bcda2
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-19 19:15:08 +0530

    Create ForEach.java

---
+++ TwoDArray/ForEach.java
@@ -0,0 +1,14 @@
+package TwoDArray;
+
+public class ForEach {
+    static void main(String[] args) {
+        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        for( int [] a : arr){
+            for(int b : a){
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
